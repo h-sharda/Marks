6b9ae75d3bd5bcdabcac7a0a851c46bc539274c1
h-sharda <harshitsharda0@gmail.com>
2024-11-25 07:42:50 +0530

    new

--- test.py
+++ test.py
@@ -3,8 +3,13 @@ import pandas as pd;
 CSV_FILE = "results.csv"
 df = pd.read_csv(CSV_FILE)
 
+average_scores = df.groupby('Course Name')['Marks'].mean()
+print(average_scores)
+
 # Group by 'Course Name' and calculate the mean score
-average_scores = df.groupby('Course Name')['Final Score (out of 100)'].mean()
+a = df.loc[df["Course Name"] == "Conservation Geography"]
+
+a = a.sort_values(by = "Marks", ascending=False)
 
 # Print the results
-print(average_scores)
+print(a)
