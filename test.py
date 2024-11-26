0c92b092c0005c543a823e6cedd293e4e4437f18
h-sharda <harshitsharda0@gmail.com>
2024-11-26 22:13:41 +0530

    updates

---
+++ test.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+df = pd.read_csv("results.csv")
+
+average = df.groupby('Course Name')['Total (100)'].mean()
+print(average, '\n\n')
+
+geo = df[df['Course Name'] == 'Conservation Geography'].sort_values(by = 'Total (100)', ascending=False)
+print(geo, '\n\n')
+
+cyber = df[df['Course Name'] == 'Cyber Security and Privacy'].sort_values(by = 'Total (100)', ascending=False)
+print(geo, '\n\n')
+
+other = df[(df['Course Name'] != 'Cyber Security and Privacy') & (df['Course Name'] != 'Conservation Geography')]
+other = other.sort_values(by = 'Total (100)', ascending=False)
+print(other, '\n\n')
+
+failed = df[(df['Exam (75)'] < 30) | (df['Assignment (25)'] < 10)]
+print(failed)
+
