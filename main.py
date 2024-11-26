3a4f32b3898e241e3832326884fbbb59c2d0a6f1
h-sharda <harshitsharda0@gmail.com>
2024-11-26 22:20:58 +0530

    change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from functions import*
 
 DRIVER_PATH = r"D:\Tools\chromedriver-win64\chromedriver.exe"
-CSV_FILE = "email.csv"
+CSV_FILE = "data.csv"
 
 results = process_all_users(CSV_FILE, DRIVER_PATH, 10)
