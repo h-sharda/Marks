65aec650566dc6a5980cca28f7362deae120621f
h-sharda <harshitsharda0@gmail.com>
2024-11-28 17:19:47 +0530

    java

--- test.py
+++ test.py
@@ -9,12 +9,14 @@ geo = df[df['Course Name'] == 'Conservation Geography'].sort_values(by = 'Total 
 print(geo, '\n\n')
 
 cyber = df[df['Course Name'] == 'Cyber Security and Privacy'].sort_values(by = 'Total (100)', ascending=False)
-print(geo, '\n\n')
+print(cyber, '\n\n')
 
-other = df[(df['Course Name'] != 'Cyber Security and Privacy') & (df['Course Name'] != 'Conservation Geography')]
+java = df[df['Course Name'] == 'Programming in Java'].sort_values(by = 'Total (100)', ascending=False)
+print(java, '\n\n')
+
+other = df[(df['Course Name'] != 'Cyber Security and Privacy') & (df['Course Name'] != 'Conservation Geography') & (df['Course Name'] != 'Programming in Java')]
 other = other.sort_values(by = 'Total (100)', ascending=False)
 print(other, '\n\n')
 
 failed = df[(df['Exam (75)'] < 30) | (df['Assignment (25)'] < 10)]
 print(failed)
-
