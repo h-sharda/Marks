c630fa20a90af2bbd679ff6c06c565a95300cfcb
Vector <harshitsharda0@gmail.com>
2025-05-21 17:07:00 +0530

    updates

--- test.py
+++ test.py
@@ -1,14 +1,17 @@
 import pandas as pd
 
-df = pd.read_csv("results.csv")
+df = pd.read_csv("result-sem-6.csv")
 
 average = df.groupby('Course Name')['Total (100)'].mean()
 print(average, '\n\n')
 
-pyscho = df[df['Course Name'] == 'Introduction To Cognitive Psychology'].sort_values(by = 'Assignment (25)', ascending=False)
+pyscho = df[df['Course Name'] == 'Introduction To Cognitive Psychology'].sort_values(by = 'Total (100)', ascending=False)
 print(pyscho, '\n\n')
 
-other = df[df['Course Name'] != 'Introduction To Cognitive Psychology'].sort_values(by = 'Total (100)', ascending=False)
+iiot = df[df['Course Name'] == 'Introduction To Internet Of Things'].sort_values(by = 'Total (100)', ascending=False)
+print(iiot, '\n\n')
+
+other = df[(df['Course Name'] != 'Introduction To Internet Of Things') & (df['Course Name'] != 'Introduction To Cognitive Psychology')].sort_values(by = 'Total (100)', ascending=False)
 print(other, '\n\n')
 
 failed = df[(df['Exam (75)'] < 30) | (df['Assignment (25)'] < 10)]
